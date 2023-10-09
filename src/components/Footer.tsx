ff09f8c824249579b1b53a6f803b99005e4ab4e5
Pee Pheelips <philipupaul@gmail.com>
2023-10-09 08:08:39 -0700

    Footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,7 @@ import { Box, Flex, Image } from "@chakra-ui/react";
 export default function Footer() {
   return (
     <>
-      <Flex justifyItems={"center"}>
-        <Box>
-          <Image src={"/logo.png"} width={8} height={10} alt="logo" />
-        </Box>
-
+      <Flex justifyItems={"center"} my={"4rem"} pl={{base:"0", md:"0", lg:"2"}}>
         <Box>Built with ❤ using chakraUI, Typescript & NextJs</Box>
       </Flex>
     </>
