9f45182923c426b75423f2c3e7ef6bb563859d9b
Pee Pheelips <philipupaul@gmail.com>
2023-10-26 09:42:25 -0700

    typo fixed

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -8,31 +8,30 @@ export default function About() {
       <Box>
         <HeadingNav name={"About"} />
         <Text>
-          In late 2017, I made a deliberate decision to delve into the world of
-          programming, and it seemed as though the universe conspired in my
-          favor. I found an opportunity to expand my knowledge and skills. I
-          embarked on this journey by joining{" "}
+          In late 2017, I decided to delve into the world of programming. I
+          found an opportunity to expand my knowledge and skills. I embarked on
+          this journey by joining{" "}
           <Text as={"span"} color={"white"} fontWeight={"semibold"}>
             {" "}
             Cixel consulting
           </Text>{" "}
-          , where I laid the foundations of web design. As time progressed, in
-          2019 during my industrial training, I began to explore Python, with a
-          particular focus on Django. <br />
+          , where I learned about the basics of web design. As time progressed,
+          in 2019 during my industrial training, I began to explore Python, with
+          a particular focus on Django. <br />
           However, 2020 brought about a major turning point when the COVID-19
-          pandemic led to lockdowns. Shortly after this challenging period, I
+          pandemic led to lockdown. Shortly after this challenging period, I
           joined{" "}
           <Text as="span" color={"white"} fontWeight={"semibold"}>
             PinnacleICTs
           </Text>
           , marking the beginning of my professional journey in the field of
-          software development. where i first got my{" "}
+          software development. where I first got my{" "}
           <Text as={"span"} color={"white"} fontWeight={"semibold"}>
             {" "}
             work experience
           </Text>{" "}
           . Fast-forward to today, and had the privilege of building software
-          for an students, various start-ups, and a other corporation. These
+          for students, various start-ups, and other corporations. These
           experiences have not only enriched my professional journey but have
           equipped me to excel in advanced Javascript and React
         </Text>
